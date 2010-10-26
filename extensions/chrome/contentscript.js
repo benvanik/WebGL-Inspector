6aff863484daba14e4febfffbba7e59a4ce08168
Ben Vanik <ben.vanik@gmail.com>
2010-10-25 19:27:39 -0700

    making progress on chrome extension injection

--- extensions/chrome/contentscript.js
+++ extensions/chrome/contentscript.js
@@ -1,5 +1,30 @@
 // Once the DOM is ready, bind to the content event
 document.addEventListener("DOMContentLoaded", function () {
+
+    var hasInjected = false;
+    function foo() {
+        if (hasInjected == false) {
+            hasInjected = true;
+
+            // TODO: add scripts
+            // TODO: add css
+            // TODO: build UI
+            alert("would inject files");
+
+            // Fake a context loss/restore
+            var resetEvent = document.createEvent("Event");
+            resetEvent.initEvent("WebGLForceResetEvent", true, true);
+            document.body.dispatchEvent(resetEvent);
+        }
+    }
+
+    chrome.extension.onRequest.addListener(function (msg) {
+        if (msg.inject == true) {
+            foo();
+        }
+        sendResponse({});
+    });
+
     document.body.addEventListener("WebGLEnabledEvent", function () {
         chrome.extension.sendRequest({}, function (response) { });
     }, false);
@@ -11,12 +36,36 @@ document.addEventListener("DOMContentLoaded", function () {
 // to canvas.getContext. When a call is made, we fire off an event that is handled in our extension
 // above (as chrome.extension.* is not available from the page).
 function main() {
+    var webglcanvases = null;
+
     // Create enabled event
-    var enabledEvent = document.createEvent("Event");
-    enabledEvent.initEvent("WebGLEnabledEvent", true, true);
     function fireEnabledEvent() {
-        var eventHost = document.body;
-        eventHost.dispatchEvent(enabledEvent);
+
+        if (webglcanvases == null) {
+            // Only setup events/etc on first enable
+            webglcanvases = [];
+
+            // Setup handling for reset
+            function resetCanvas(canvas) {
+                var lostEvent = document.createEvent("Event");
+                lostEvent.initEvent("webglcontextlost", true, true);
+                canvas.dispatchEvent(lostEvent);
+                var restoreEvent = document.createEvent("Event");
+                restoreEvent.initEvent("webglcontextrestored", true, true);
+                canvas.dispatchEvent(restoreEvent);
+            };
+
+            // Listen for reset events
+            document.body.addEventListener("WebGLForceResetEvent", function () {
+                for (var n = 0; n < webglcanvases.length; n++) {
+                    resetCanvas(webglcanvases[n]);
+                }
+            }, false);
+        }
+
+        var enabledEvent = document.createEvent("Event");
+        enabledEvent.initEvent("WebGLEnabledEvent", true, true);
+        document.body.dispatchEvent(enabledEvent);
     };
 
     // Rewrite getContext to snoop for webgl
@@ -24,9 +73,11 @@ function main() {
     HTMLCanvasElement.prototype.getContext = function () {
         if (arguments[0] == "experimental-webgl") {
             // Page is requesting a WebGL context!
-            fireEnabledEvent();
+            fireEnabledEvent(this);
+            if (webglcanvases.indexOf(this) == -1) {
+                webglcanvases.push(this);
+            }
         }
-
         return originalGetContext.apply(this, arguments);
     };
 }
