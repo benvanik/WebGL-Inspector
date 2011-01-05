79518de5106c02847729b94c670a821454aefe03
Ben Vanik <ben.vanik@gmail.com>
2011-01-04 21:05:02 -0800

    more reliable way to set redundant call css

--- core/ui/tabs/textures/TexturesTab.js
+++ core/ui/tabs/textures/TexturesTab.js
@@ -13,7 +13,7 @@
         '                <!-- inspector -->' +
         '            </div>' +
         '            <div class="surface-inspector-statusbar">' +
-        '                <canvas class="surface-inspector-pixel" width="1" height="1"></canvas>&nbsp;'+
+        '                <canvas class="gli-reset surface-inspector-pixel" width="1" height="1"></canvas>&nbsp;' +
         '                <span class="surface-inspector-location"></span>' +
         '            </div>' +
         '        </div>' +
