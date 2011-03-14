0128843dc97115a5bad39bc360808773c96957a5
unknown <benvanik@.ad.corp.google.com>
2011-03-14 14:38:44 -0700

    css reset on popup <body> to fix inconsistencies

--- core/host/HostUI.js
+++ core/host/HostUI.js
@@ -90,7 +90,7 @@
         var startupWidth = gli.settings.session.hudPopupWidth ? gli.settings.session.hudPopupWidth : 1000;
         var startupHeight = gli.settings.session.hudPopupHeight ? gli.settings.session.hudPopupHeight : 500;
         var w = this.browserWindow = window.open("about:blank", "_blank", "location=no,menubar=no,scrollbars=no,status=no,toolbar=no,innerWidth=" + startupWidth + ",innerHeight=" + startupHeight);
-        w.document.writeln("<html><head><title>WebGL Inspector</title></head><body style='margin: 0px; padding: 0px;'></body></html>");
+        w.document.writeln("<html><head><title>WebGL Inspector</title></head><body class='yui3-cssreset' style='margin: 0px; padding: 0px;'></body></html>");
 
         w.addEventListener("unload", function () {
             context.window.browserWindow.opener.focus();
