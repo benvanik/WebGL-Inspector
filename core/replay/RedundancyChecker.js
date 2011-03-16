ff4d453617dcd5848ff5e2cd1ef741bc48cc2395
Ben Vanik <ben.vanik@gmail.com>
2011-03-15 19:18:28 -0700

    uniform redundancy checks

--- core/replay/RedundancyChecker.js
+++ core/replay/RedundancyChecker.js
@@ -10,6 +10,18 @@
         };
         this.canvas = document.createElement("canvas");
         var gl = this.gl = prepareCanvas(this.canvas);
+        
+        // Cache off uniform name so that we can retrieve it later
+        var original_getUniformLocation = gl.getUniformLocation;
+        gl.getUniformLocation = function () {
+            var result = original_getUniformLocation.apply(gl, arguments);
+            if (result) {
+                var tracked = arguments[0].trackedObject;
+                result.sourceProgram = tracked;
+                result.sourceUniformName = arguments[1];
+            }
+            return result;
+        };
     };
     
     var stateCacheModifiers = {
@@ -33,7 +45,7 @@
             this.stateCache["RENDERBUFFER_BINDING"] = renderbuffer;
         },
         bindTexture: function (target, texture) {
-            var activeTexture = this.stateCache["ACTIVE_TEXTURE"];
+            var activeTexture = (this.stateCache["ACTIVE_TEXTURE"] - this.TEXTURE0);
             switch (target) {
                 case this.TEXTURE_2D:
                     this.stateCache["TEXTURE_BINDING_2D_" + activeTexture] = texture;
@@ -270,6 +282,75 @@
                     break;
             }
         },
+        uniformN: function (location, v) {
+            var program = location.sourceProgram;
+            if (v.slice !== undefined) {
+                v = v.slice();
+            } else {
+                v = new Float32Array(v);
+            }
+            program.uniformCache[location.sourceUniformName] = v;
+        },
+        uniform1f: function (location, v0) {
+            stateCacheModifiers.uniformN.call(this, location, [v0]);
+        },
+        uniform2f: function (location, v0, v1) {
+            stateCacheModifiers.uniformN.call(this, location, [v0, v1]);
+        },
+        uniform3f: function (location, v0, v1, v2) {
+            stateCacheModifiers.uniformN.call(this, location, [v0, v1, v2]);
+        },
+        uniform4f: function (location, v0, v1, v2, v3) {
+            stateCacheModifiers.uniformN.call(this, location, [v0, v1, v2, v3]);
+        },
+        uniform1i: function (location, v0) {
+            stateCacheModifiers.uniformN.call(this, location, [v0]);
+        },
+        uniform2i: function (location, v0, v1) {
+            stateCacheModifiers.uniformN.call(this, location, [v0, v1]);
+        },
+        uniform3i: function (location, v0, v1, v2) {
+            stateCacheModifiers.uniformN.call(this, location, [v0, v1, v2]);
+        },
+        uniform4i: function (location, v0, v1, v2, v3) {
+            stateCacheModifiers.uniformN.call(this, location, [v0, v1, v2, v3]);
+        },
+        uniform1fv: function (location, v) {
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniform2fv: function (location, v) {
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniform3fv: function (location, v) {
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniform4fv: function (location, v) {
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniform1iv: function (location, v) {
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniform2iv: function (location, v) {
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniform3iv: function (location, v) {
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniform4iv: function (location, v) {
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniformMatrix2fv: function (location, transpose, v) {
+            // TODO: transpose
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniformMatrix3fv: function (location, transpose, v) {
+            // TODO: transpose
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
+        uniformMatrix4fv: function (location, transpose, v) {
+            // TODO: transpose
+            stateCacheModifiers.uniformN.call(this, location, v);
+        },
         useProgram: function (program) {
             this.stateCache["CURRENT_PROGRAM"] = program;
         },
@@ -329,7 +410,7 @@
             return this.stateCache["RENDERBUFFER_BINDING"] == renderbuffer;
         },
         bindTexture: function (target, texture) {
-            var activeTexture = this.stateCache["ACTIVE_TEXTURE"];
+            var activeTexture = (this.stateCache["ACTIVE_TEXTURE"] - this.TEXTURE0);
             switch (target) {
                 case this.TEXTURE_2D:
                     return this.stateCache["TEXTURE_BINDING_2D_" + activeTexture] == texture;
@@ -500,6 +581,71 @@
                            (this.stateCache["STENCIL_BACK_FAIL"] == fail) && (this.stateCache["STENCIL_BACK_PASS_DEPTH_FAIL"] == zfail) && (this.stateCache["STENCIL_BACK_PASS_DEPTH_PASS"] == zpass);
             }
         },
+        uniformN: function (location, v) {
+            var program = location.sourceProgram;
+            if (!program.uniformCache) return false;
+            return gli.util.arrayCompare(program.uniformCache[location.sourceUniformName], v);
+        },
+        uniform1f: function (location, v0) {
+            return redundantChecks.uniformN.call(this, location, [v0]);
+        },
+        uniform2f: function (location, v0, v1) {
+            return redundantChecks.uniformN.call(this, location, [v0, v1]);
+        },
+        uniform3f: function (location, v0, v1, v2) {
+            return redundantChecks.uniformN.call(this, location, [v0, v1, v2]);
+        },
+        uniform4f: function (location, v0, v1, v2, v3) {
+            return redundantChecks.uniformN.call(this, location, [v0, v1, v2, v3]);
+        },
+        uniform1i: function (location, v0) {
+            return redundantChecks.uniformN.call(this, location, [v0]);
+        },
+        uniform2i: function (location, v0, v1) {
+            return redundantChecks.uniformN.call(this, location, [v0, v1]);
+        },
+        uniform3i: function (location, v0, v1, v2) {
+            return redundantChecks.uniformN.call(this, location, [v0, v1, v2]);
+        },
+        uniform4i: function (location, v0, v1, v2, v3) {
+            return redundantChecks.uniformN.call(this, location, [v0, v1, v2, v3]);
+        },
+        uniform1fv: function (location, v) {
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniform2fv: function (location, v) {
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniform3fv: function (location, v) {
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniform4fv: function (location, v) {
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniform1iv: function (location, v) {
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniform2iv: function (location, v) {
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniform3iv: function (location, v) {
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniform4iv: function (location, v) {
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniformMatrix2fv: function (location, transpose, v) {
+            // TODO: transpose
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniformMatrix3fv: function (location, transpose, v) {
+            // TODO: transpose
+            return redundantChecks.uniformN.call(this, location, v);
+        },
+        uniformMatrix4fv: function (location, transpose, v) {
+            // TODO: transpose
+            return redundantChecks.uniformN.call(this, location, v);
+        },
         useProgram: function (program) {
             return this.stateCache["CURRENT_PROGRAM"] == program;
         },
@@ -543,7 +689,7 @@
     RedundancyChecker.prototype.initializeStateCache = function (gl) {
         var stateCache = {};
         
-        var stateParameters = ["ACTIVE_TEXTURE", "ARRAY_BUFFER_BINDING", "BLEND", "BLEND_COLOR", "BLEND_DST_ALPHA", "BLEND_DST_RGB", "BLEND_EQUATION_ALPHA", "BLEND_EQUATION_RGB", "BLEND_SRC_ALPHA", "BLEND_SRC_RGB", "COLOR_CLEAR_VALUE", "COLOR_WRITEMASK", "CULL_FACE", "CULL_FACE_MODE", "DEPTH_FUNC", "DEPTH_RANGE", "DEPTH_WRITEMASK", "ELEMENT_ARRAY_BUFFER_BINDING", "FRAMEBUFFER_BINDING", "FRONT_FACE", "GENERATE_MIPMAP_HINT", "LINE_WIDTH", "PACK_ALIGNMENT", "POLYGON_OFFSET_FACTOR", "POLYGON_OFFSET_FILL", "POLYGON_OFFSET_UNITS", "RENDERBUFFER_BINDING", "POLYGON_OFFSET_FACTOR", "POLYGON_OFFSET_FILL", "POLYGON_OFFSET_UNITS", "SAMPLE_ALPHA_TO_COVERAGE", "SAMPLE_COVERAGE", "SAMPLE_COVERAGE_INVERT", "SAMPLE_COVERAGE_VALUE", "SCISSOR_BOX", "SCISSOR_TEST", "STENCIL_BACK_FAIL", "STENCIL_BACK_FUNC", "STENCIL_BACK_PASS_DEPTH_FAIL", "STENCIL_BACK_PASS_DEPTH_PASS", "STENCIL_BACK_REF", "STENCIL_BACK_VALUE_MASK", "STENCIL_BACK_WRITEMASK", "STENCIL_CLEAR_VALUE", "STENCIL_FAIL", "STENCIL_FUNC", "STENCIL_PASS_DEPTH_FAIL", "STENCIL_PASS_DEPTH_PASS", "STENCIL_REF", "STENCIL_TEST", "STENCIL_VALUE_MASK", "STENCIL_WRITEMASK", "UNPACK_ALIGNMENT", "UNPACK_COLORSPACE_CONVERSION_WEBGL", "UNPACK_FLIP_Y_WEBGL", "UNPACK_PREMULTIPLY_ALPHA_WEBGL", "VIEWPORT"];
+        var stateParameters = ["ACTIVE_TEXTURE", "ARRAY_BUFFER_BINDING", "BLEND", "BLEND_COLOR", "BLEND_DST_ALPHA", "BLEND_DST_RGB", "BLEND_EQUATION_ALPHA", "BLEND_EQUATION_RGB", "BLEND_SRC_ALPHA", "BLEND_SRC_RGB", "COLOR_CLEAR_VALUE", "COLOR_WRITEMASK", "CULL_FACE", "CULL_FACE_MODE", "CURRENT_PROGRAM", "DEPTH_FUNC", "DEPTH_RANGE", "DEPTH_WRITEMASK", "ELEMENT_ARRAY_BUFFER_BINDING", "FRAMEBUFFER_BINDING", "FRONT_FACE", "GENERATE_MIPMAP_HINT", "LINE_WIDTH", "PACK_ALIGNMENT", "POLYGON_OFFSET_FACTOR", "POLYGON_OFFSET_FILL", "POLYGON_OFFSET_UNITS", "RENDERBUFFER_BINDING", "POLYGON_OFFSET_FACTOR", "POLYGON_OFFSET_FILL", "POLYGON_OFFSET_UNITS", "SAMPLE_ALPHA_TO_COVERAGE", "SAMPLE_COVERAGE", "SAMPLE_COVERAGE_INVERT", "SAMPLE_COVERAGE_VALUE", "SCISSOR_BOX", "SCISSOR_TEST", "STENCIL_BACK_FAIL", "STENCIL_BACK_FUNC", "STENCIL_BACK_PASS_DEPTH_FAIL", "STENCIL_BACK_PASS_DEPTH_PASS", "STENCIL_BACK_REF", "STENCIL_BACK_VALUE_MASK", "STENCIL_BACK_WRITEMASK", "STENCIL_CLEAR_VALUE", "STENCIL_FAIL", "STENCIL_FUNC", "STENCIL_PASS_DEPTH_FAIL", "STENCIL_PASS_DEPTH_PASS", "STENCIL_REF", "STENCIL_TEST", "STENCIL_VALUE_MASK", "STENCIL_WRITEMASK", "UNPACK_ALIGNMENT", "UNPACK_COLORSPACE_CONVERSION_WEBGL", "UNPACK_FLIP_Y_WEBGL", "UNPACK_PREMULTIPLY_ALPHA_WEBGL", "VIEWPORT"];
         for (var n = 0; n < stateParameters.length; n++) {
             try {
                 stateCache[stateParameters[n]] = gl.getParameter(gl[stateParameters[n]]);
@@ -574,6 +720,59 @@
         return stateCache;
     };
     
+    RedundancyChecker.prototype.cacheUniformValues = function (gl, frame) {
+        var originalProgram = gl.getParameter(gl.CURRENT_PROGRAM);
+
+        for (var n = 0; n < frame.uniformValues.length; n++) {
+            var program = frame.uniformValues[n].program;
+            var values = frame.uniformValues[n].values;
+
+            var target = program.mirror.target;
+            if (!target) {
+                continue;
+            }
+            
+            program.uniformCache = {};
+
+            gl.useProgram(target);
+
+            for (var name in values) {
+                var data = values[name];
+                var loc = gl.getUniformLocation(target, name);
+
+                switch (data.type) {
+                    case gl.FLOAT:
+                    case gl.FLOAT_VEC2:
+                    case gl.FLOAT_VEC3:
+                    case gl.FLOAT_VEC4:
+                    case gl.INT:
+                    case gl.INT_VEC2:
+                    case gl.INT_VEC3:
+                    case gl.INT_VEC4:
+                    case gl.BOOL:
+                    case gl.BOOL_VEC2:
+                    case gl.BOOL_VEC3:
+                    case gl.BOOL_VEC4:
+                    case gl.SAMPLER_2D:
+                    case gl.SAMPLER_CUBE:
+                        if (data.value && data.value.length !== undefined) {
+                            program.uniformCache[name] = data.value;
+                        } else {
+                            program.uniformCache[name] = [data.value];
+                        }
+                        break;
+                    case gl.FLOAT_MAT2:
+                    case gl.FLOAT_MAT3:
+                    case gl.FLOAT_MAT4:
+                        program.uniformCache[name] = data.value;
+                        break;
+                }
+            }
+        }
+
+        gl.useProgram(originalProgram);
+    };
+    
     RedundancyChecker.prototype.run = function (frame) {
         // TODO: if we every support editing, we may want to recheck
         if (frame.hasCheckedRedundancy) {
@@ -590,6 +789,9 @@
         
         // Setup initial state cache (important to do here so we have the frame initial state)
         gl.stateCache = this.initializeStateCache(gl);
+        
+        // Cache all uniform values for checking
+        this.cacheUniformValues(gl, frame);
         
         var redundantCalls = 0;
         var calls = frame.calls;
