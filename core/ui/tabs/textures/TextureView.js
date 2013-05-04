bd3d9b50cd601bf871f92978687e42f7f49ec204
Jan Scheurer <lj1102@lj1102-UX31A.(none)>
2013-05-04 19:31:15 +0200

    Add support for UNSIGNED_SHORT_4_4_4_4 texture type

--- core/ui/tabs/textures/TextureView.js
+++ core/ui/tabs/textures/TextureView.js
@@ -195,8 +195,23 @@
                 console.log("todo: UNSIGNED_SHORT_5_6_5");
                 return null;
             case gl.UNSIGNED_SHORT_4_4_4_4:
-                console.log("todo: UNSIGNED_SHORT_4_4_4_4");
-                return null;
+                if (format == gl.RGBA) {
+                    var strideDiff = (width * 4) % unpackAlignment, x, y, binval;
+                    for (y = 0; y < height; y++) {
+                        for (x = 0; x < width; x++, sn++, dn += 4) {
+                            binval = source[sn];
+                            imageData.data[dn + 0] = binval >> 12;
+                            imageData.data[dn + 1] = (binval >> 8) & 15;
+                            imageData.data[dn + 2] = (binval >> 4) & 15;
+                            imageData.data[dn + 3] = binval & 15;
+                        }
+                        sn += strideDiff;
+                    }
+                } else {
+                    console.log("unsupported texture format");
+                    return null;
+                }
+                break;
             case gl.UNSIGNED_SHORT_5_5_5_1:
                 console.log("todo: UNSIGNED_SHORT_5_5_5_1");
                 return null;
