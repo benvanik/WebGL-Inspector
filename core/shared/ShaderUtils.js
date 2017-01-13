7e47d7428888ab9e9eaee38974b6bb60f03d3fce
Gregg Tavares <github@greggman.com>
2017-01-13 20:20:44 +0900

    Change to 4 space indentation to match style

--- core/shared/ShaderUtils.js
+++ core/shared/ShaderUtils.js
@@ -8,193 +8,193 @@ define([
      * Returns the corresponding bind point for a given sampler type
      */
     function getBindPointForSamplerType(gl, type) {
-      return typeMap[type].bindPoint;
+        return typeMap[type].bindPoint;
     }
 
     // This kind of sucks! If you could compose functions as in `var fn = gl[name];`
     // this code could be a lot smaller but that is sadly really slow (T_T)
 
     function floatSetter(gl, location) {
-      return function(v) {
-        gl.uniform1f(location, v);
-      };
+        return function(v) {
+            gl.uniform1f(location, v);
+        };
     }
 
     function floatArraySetter(gl, location) {
-      return function(v) {
-        gl.uniform1fv(location, v);
-      };
+        return function(v) {
+            gl.uniform1fv(location, v);
+        };
     }
 
     function floatVec2Setter(gl, location) {
-      return function(v) {
-        gl.uniform2fv(location, v);
-      };
+        return function(v) {
+            gl.uniform2fv(location, v);
+        };
     }
 
     function floatVec3Setter(gl, location) {
-      return function(v) {
-        gl.uniform3fv(location, v);
-      };
+        return function(v) {
+            gl.uniform3fv(location, v);
+        };
     }
 
     function floatVec4Setter(gl, location) {
-      return function(v) {
-        gl.uniform4fv(location, v);
-      };
+        return function(v) {
+          gl.uniform4fv(location, v);
+        };
     }
 
     function intSetter(gl, location) {
-      return function(v) {
-        gl.uniform1i(location, v);
-      };
+        return function(v) {
+            gl.uniform1i(location, v);
+        };
     }
 
     function intArraySetter(gl, location) {
-      return function(v) {
-        gl.uniform1iv(location, v);
-      };
+        return function(v) {
+            gl.uniform1iv(location, v);
+        };
     }
 
     function intVec2Setter(gl, location) {
-      return function(v) {
-        gl.uniform2iv(location, v);
-      };
+        return function(v) {
+            gl.uniform2iv(location, v);
+        };
     }
 
     function intVec3Setter(gl, location) {
-      return function(v) {
-        gl.uniform3iv(location, v);
-      };
+        return function(v) {
+            gl.uniform3iv(location, v);
+        };
     }
 
     function intVec4Setter(gl, location) {
-      return function(v) {
-        gl.uniform4iv(location, v);
-      };
+        return function(v) {
+            gl.uniform4iv(location, v);
+        };
     }
 
     function uintSetter(gl, location) {
-      return function(v) {
-        gl.uniform1ui(location, v);
-      };
+        return function(v) {
+            gl.uniform1ui(location, v);
+        };
     }
 
     function uintArraySetter(gl, location) {
-      return function(v) {
-        gl.uniform1uiv(location, v);
-      };
+        return function(v) {
+            gl.uniform1uiv(location, v);
+        };
     }
 
     function uintVec2Setter(gl, location) {
-      return function(v) {
-        gl.uniform2uiv(location, v);
-      };
+        return function(v) {
+            gl.uniform2uiv(location, v);
+        };
     }
 
     function uintVec3Setter(gl, location) {
-      return function(v) {
-        gl.uniform3uiv(location, v);
-      };
+        return function(v) {
+            gl.uniform3uiv(location, v);
+        };
     }
 
     function uintVec4Setter(gl, location) {
-      return function(v) {
-        gl.uniform4uiv(location, v);
-      };
+        return function(v) {
+            gl.uniform4uiv(location, v);
+        };
     }
 
     function floatMat2Setter(gl, location) {
-      return function(v) {
-        gl.uniformMatrix2fv(location, false, v);
-      };
+        return function(v) {
+            gl.uniformMatrix2fv(location, false, v);
+        };
     }
 
     function floatMat3Setter(gl, location) {
-      return function(v) {
-        gl.uniformMatrix3fv(location, false, v);
-      };
+        return function(v) {
+            gl.uniformMatrix3fv(location, false, v);
+        };
     }
 
     function floatMat4Setter(gl, location) {
-      return function(v) {
-        gl.uniformMatrix4fv(location, false, v);
-      };
+        return function(v) {
+            gl.uniformMatrix4fv(location, false, v);
+        };
     }
 
     function floatMat23Setter(gl, location) {
-      return function(v) {
-        gl.uniformMatrix2x3fv(location, false, v);
-      };
+        return function(v) {
+            gl.uniformMatrix2x3fv(location, false, v);
+        };
     }
 
     function floatMat32Setter(gl, location) {
-      return function(v) {
-        gl.uniformMatrix3x2fv(location, false, v);
-      };
+        return function(v) {
+            gl.uniformMatrix3x2fv(location, false, v);
+        };
     }
 
     function floatMat24Setter(gl, location) {
-      return function(v) {
-        gl.uniformMatrix2x4fv(location, false, v);
-      };
+        return function(v) {
+            gl.uniformMatrix2x4fv(location, false, v);
+        };
     }
 
     function floatMat42Setter(gl, location) {
-      return function(v) {
-        gl.uniformMatrix4x2fv(location, false, v);
-      };
+        return function(v) {
+            gl.uniformMatrix4x2fv(location, false, v);
+        };
     }
 
     function floatMat34Setter(gl, location) {
-      return function(v) {
-        gl.uniformMatrix3x4fv(location, false, v);
-      };
+        return function(v) {
+            gl.uniformMatrix3x4fv(location, false, v);
+        };
     }
 
     function floatMat43Setter(gl, location) {
-      return function(v) {
-        gl.uniformMatrix4x3fv(location, false, v);
-      };
+        return function(v) {
+            gl.uniformMatrix4x3fv(location, false, v);
+        };
     }
 
     function samplerSetter(gl, type, unit, location) {
-      var bindPoint = getBindPointForSamplerType(gl, type);
-      return function(textureOrPair) {
-        let texture;
-        if (textureOrPair instanceof WebGLTexture) {
-          texture = textureOrPair;
-        } else {
-          texture = textureOrPair.texture;
-          gl.bindSampler(unit, textureOrPair.sampler);
-        }
-        gl.uniform1i(location, unit);
-        gl.activeTexture(gl.TEXTURE0 + unit);
-        gl.bindTexture(bindPoint, texture);
-      };
+        var bindPoint = getBindPointForSamplerType(gl, type);
+        return function(textureOrPair) {
+            let texture;
+            if (textureOrPair instanceof WebGLTexture) {
+                texture = textureOrPair;
+            } else {
+                texture = textureOrPair.texture;
+                gl.bindSampler(unit, textureOrPair.sampler);
+            }
+            gl.uniform1i(location, unit);
+            gl.activeTexture(gl.TEXTURE0 + unit);
+            gl.bindTexture(bindPoint, texture);
+        };
     }
 
     function samplerArraySetter(gl, type, unit, location, size) {
-      var bindPoint = getBindPointForSamplerType(gl, type);
-      var units = new Int32Array(size);
-      for (var ii = 0; ii < size; ++ii) {
-        units[ii] = unit + ii;
-      }
+        var bindPoint = getBindPointForSamplerType(gl, type);
+        var units = new Int32Array(size);
+        for (var ii = 0; ii < size; ++ii) {
+            units[ii] = unit + ii;
+        }
 
-      return function(textures) {
-        gl.uniform1iv(location, units);
-        textures.forEach(function(textureOrPair, index) {
-          gl.activeTexture(gl.TEXTURE0 + units[index]);
-          let texture;
-          if (textureOrPair instanceof WebGLTexture) {
-            texture = textureOrPair;
-          } else {
-            texture = textureOrPair.texture;
-            gl.bindSampler(unit, textureOrPair.sampler);
-          }
-          gl.bindTexture(bindPoint, texture);
-        });
-      };
+        return function(textures) {
+            gl.uniform1iv(location, units);
+            textures.forEach(function(textureOrPair, index) {
+                gl.activeTexture(gl.TEXTURE0 + units[index]);
+                let texture;
+                if (textureOrPair instanceof WebGLTexture) {
+                    texture = textureOrPair;
+                } else {
+                    texture = textureOrPair.texture;
+                    gl.bindSampler(unit, textureOrPair.sampler);
+                }
+                gl.bindTexture(bindPoint, texture);
+            });
+        };
     }
 
     const typeMap = {};
@@ -240,43 +240,52 @@ define([
     typeMap[glc.UNSIGNED_INT_SAMPLER_2D_ARRAY] = { Type: null,         size:  0, setter: samplerSetter,    arraySetter: samplerArraySetter, bindPoint: glc.TEXTURE_2D_ARRAY, };
 
     function floatAttribSetter(gl, index) {
-      return function(b) {
-        gl.bindBuffer(gl.ARRAY_BUFFER, b.buffer);
-        gl.enableVertexAttribArray(index);
-        gl.vertexAttribPointer(
-            index, b.numComponents || b.size, b.type || gl.FLOAT, b.normalize || false, b.stride || 0, b.offset || 0);
-      };
+        return function(b) {
+            gl.bindBuffer(gl.ARRAY_BUFFER, b.buffer);
+            gl.enableVertexAttribArray(index);
+            gl.vertexAttribPointer(
+                index, b.numComponents || b.size, b.type || gl.FLOAT, b.normalize || false, b.stride || 0, b.offset || 0);
+        };
     }
 
     function intAttribSetter(gl, index) {
-      return function(b) {
-        gl.bindBuffer(gl.ARRAY_BUFFER, b.buffer);
-        gl.enableVertexAttribArray(index);
-        gl.vertexAttribIPointer(
-            index, b.numComponents || b.size, b.type || gl.INT, b.stride || 0, b.offset || 0);
-      };
+        return function(b) {
+            gl.bindBuffer(gl.ARRAY_BUFFER, b.buffer);
+            gl.enableVertexAttribArray(index);
+            gl.vertexAttribIPointer(
+                index, b.numComponents || b.size, b.type || gl.INT, b.stride || 0, b.offset || 0);
+        };
     }
 
     function matAttribSetter(gl, index, typeInfo) {
-      var defaultSize = typeInfo.size;
-      var count = typeInfo.count;
+        var defaultSize = typeInfo.size;
+        var count = typeInfo.count;
 
-      return function(b) {
-        gl.bindBuffer(gl.ARRAY_BUFFER, b.buffer);
-        var numComponents = b.size || b.numComponents || defaultSize;
-        var size = numComponents / count;
-        var type = b.type || gl.FLOAT;
-        var typeInfo = typeMap[type];
-        var stride = typeInfo.size * numComponents;
-        var normalize = b.normalize || false;
-        var offset = b.offset || 0;
-        var rowOffset = stride / count;
-        for (var i = 0; i < count; ++i) {
-          gl.enableVertexAttribArray(index + i);
-          gl.vertexAttribPointer(
-              index + i, size, type, normalize, stride, offset + rowOffset * i);
-        }
-      };
+        return function(b) {
+            gl.bindBuffer(gl.ARRAY_BUFFER, b.buffer);
+            var numComponents = b.size || b.numComponents || defaultSize;
+            var size = numComponents / count;
+            var type = b.type || gl.FLOAT;
+            var typeInfo = typeMap[type];
+            var stride = typeInfo.size * numComponents;
+            var normalize = b.normalize || false;
+            var offset = b.offset || 0;
+            var rowOffset = stride / count;
+            for (var i = 0; i < count; ++i) {
+                gl.enableVertexAttribArray(index + i);
+                gl.vertexAttribPointer(
+                    index + i, size, type, normalize, stride, offset + rowOffset * i);
+            }
+        };
+    }
+
+    function addLineNumbers(src, lineOffset) {
+        lineOffset = lineOffset || 0;
+        ++lineOffset;
+
+        return src.split("\n").map(function(line, ndx) {
+            return (ndx + lineOffset) + ": " + line;
+        }).join("\n");
     }
 
     function createShader(gl, vSrc, vType) {
@@ -285,7 +294,7 @@ define([
         gl.compileShader(shader);
         const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
         if (!success) {
-            console.error(gl.getShaderInfoLog(shader));
+            console.error(addLineNumbers(vSrc), "\n", gl.getShaderInfoLog(shader));
         }
         return shader;
     };
@@ -312,90 +321,95 @@ define([
         shaders.forEach(shader => gl.deleteShader(shader));
         var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
         if (!linked) {
-            console.error(gl.getProgramInfoLog(program));
+            console.error("--vertexShader--\n",
+                          addLineNumbers(source[0]),
+                          "--fragmentShader--\n",
+                          addLineNumbers(source[1]),
+                          "--error--\n",
+                          gl.getProgramInfoLog(program));
         }
         return program;
     }
 
     function createUniformSetters(gl, program) {
-      var textureUnit = 0;
+        var textureUnit = 0;
 
-      function createUniformSetter(program, uniformInfo) {
-        var location = gl.getUniformLocation(program, uniformInfo.name);
-        var isArray = (uniformInfo.size > 1 && uniformInfo.name.substr(-3) === "[0]");
-        var type = uniformInfo.type;
-        var typeInfo = typeMap[type];
-        if (!typeInfo) {
-          throw ("unknown type: 0x" + type.toString(16)); // we should never get here.
-        }
-        if (typeInfo.bindPoint) {
-          // it's a sampler
-          var unit = textureUnit;
-          textureUnit += uniformInfo.size;
+        function createUniformSetter(program, uniformInfo) {
+            var location = gl.getUniformLocation(program, uniformInfo.name);
+            var isArray = (uniformInfo.size > 1 && uniformInfo.name.substr(-3) === "[0]");
+            var type = uniformInfo.type;
+            var typeInfo = typeMap[type];
+            if (!typeInfo) {
+                throw ("unknown type: 0x" + type.toString(16)); // we should never get here.
+            }
+            if (typeInfo.bindPoint) {
+                // it's a sampler
+                var unit = textureUnit;
+                textureUnit += uniformInfo.size;
 
-          if (isArray) {
-            return typeInfo.arraySetter(gl, type, unit, location, uniformInfo.size);
-          } else {
-            return typeInfo.setter(gl, type, unit, location, uniformInfo.size);
-          }
-        } else {
-          if (typeInfo.arraySetter && isArray) {
-            return typeInfo.arraySetter(gl, location);
-          } else {
-            return typeInfo.setter(gl, location);
-          }
+                if (isArray) {
+                    return typeInfo.arraySetter(gl, type, unit, location, uniformInfo.size);
+                } else {
+                    return typeInfo.setter(gl, type, unit, location, uniformInfo.size);
+                }
+            } else {
+                if (typeInfo.arraySetter && isArray) {
+                    return typeInfo.arraySetter(gl, location);
+                } else {
+                    return typeInfo.setter(gl, location);
+                }
+            }
         }
-      }
 
-      var uniformSetters = { };
-      var numUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+        var uniformSetters = { };
+        var numUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
 
-      for (var ii = 0; ii < numUniforms; ++ii) {
-        var uniformInfo = gl.getActiveUniform(program, ii);
-        if (!uniformInfo) {
-          break;
+        for (var ii = 0; ii < numUniforms; ++ii) {
+            var uniformInfo = gl.getActiveUniform(program, ii);
+            if (!uniformInfo) {
+                break;
+            }
+            var name = uniformInfo.name;
+            // remove the array suffix.
+            if (name.substr(-3) === "[0]") {
+                name = name.substr(0, name.length - 3);
+            }
+            var setter = createUniformSetter(program, uniformInfo);
+            uniformSetters[name] = setter;
         }
-        var name = uniformInfo.name;
-        // remove the array suffix.
-        if (name.substr(-3) === "[0]") {
-          name = name.substr(0, name.length - 3);
-        }
-        var setter = createUniformSetter(program, uniformInfo);
-        uniformSetters[name] = setter;
-      }
-      return uniformSetters;
+        return uniformSetters;
     }
 
     function setUniforms(gl, setters, values) {  // eslint-disable-line
-      var actualSetters = setters.uniformSetters || setters;
-      var numArgs = arguments.length;
-      for (var andx = 1; andx < numArgs; ++andx) {
-        var vals = arguments[andx];
-        if (Array.isArray(vals)) {
-          var numValues = vals.length;
-          for (var ii = 0; ii < numValues; ++ii) {
-            setUniforms(actualSetters, vals[ii]);
-          }
-        } else {
-          for (var name in vals) {
-            var setter = actualSetters[name];
-            if (setter) {
-              setter(vals[name]);
+        var actualSetters = setters.uniformSetters || setters;
+        var numArgs = arguments.length;
+        for (var andx = 1; andx < numArgs; ++andx) {
+            var vals = arguments[andx];
+            if (Array.isArray(vals)) {
+                var numValues = vals.length;
+                for (var ii = 0; ii < numValues; ++ii) {
+                    setUniforms(actualSetters, vals[ii]);
+                }
+            } else {
+                for (var name in vals) {
+                    var setter = actualSetters[name];
+                    if (setter) {
+                        setter(vals[name]);
+                    }
+                }
             }
-          }
         }
-      }
     }
 
     function createProgramInfo(gl, sources, attributes) {
-      const program = createProgram(gl, sources, attributes);
-      const uniformSetters = createUniformSetters(gl, program);
-      const programInfo = {
-        program: program,
-        uniformSetters: uniformSetters,
-      };
+        const program = createProgram(gl, sources, attributes);
+        const uniformSetters = createUniformSetters(gl, program);
+        const programInfo = {
+            program: program,
+            uniformSetters: uniformSetters,
+        };
 
-      return programInfo;
+        return programInfo;
     }
 
     return {
